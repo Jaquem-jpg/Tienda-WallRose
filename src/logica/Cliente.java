ddee6c8eb008b5fe431895f8b17b53e0502cc4fd
Jaquem <jaquemog2017@gmail.com>
2026-05-07 21:28:25 -0600

    Creamos las clases de logica.

--- src/logica/Cliente.java
+++ src/logica/Cliente.java
@@ -1,5 +1,49 @@
 package logica;
 
 public class Cliente {
+	private String idCliente;
+	private String email;
+	private String nombre;
+	
+	// Constructor
+	public Cliente(	String idCliente, String email, String nombre) {
+		this.idCliente = idCliente;
+		this.email = email;
+		this.nombre = nombre;
+	}
+	
+	
+	// Getters y Setters
+	public String getIdCliente() {
+		return idCliente;
+	}
 
+	public void setCliente(String idCliente) {
+		this.idCliente = idCliente;
+	}
+	
+	public String getNombre() {
+		return nombre;
+	}
+	
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	
+	public String getEmail() {
+		return email;
+	}
+	
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	
+	public String toString() {
+		return "Cliente{" + "id=" + idCliente + ", nombre=" + nombre + ", email=" + email + '}';
+	}
+	
 }
+
+
+
--- src/logica/Producto.java
+++ src/logica/Producto.java
@@ -1,5 +1,26 @@
 package logica;
 
 public class Producto {
-
+	static int codigo;
+	private  int codigoProducto;
+	private float existencia;
+	private String nombre;
+	private int numero;
+	private double precio;
+	private String unidad;
+	
+	
+	// Constructor
+	public Producto(int codigoProducto, float existencia, String nombre, int numero, double precio, String unidad) {
+		this.codigoProducto = codigoProducto;
+		this.existencia = existencia;
+		this.nombre = nombre;
+		this.numero =  numero;
+		this.precio = precio;
+		this.unidad = unidad;
+	}
+	
+	
+	//Getters y Setters
+	
 }
