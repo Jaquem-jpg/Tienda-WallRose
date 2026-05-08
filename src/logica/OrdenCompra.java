6ecf325b8b723bf7f2db9ca1a9d2ebcc4066f446
Jaquem <jaquemog2017@gmail.com>
2026-05-07 22:08:34 -0600

    Clase OrdenCompra con atributos, getters y setters

--- src/logica/OrdenCompra.java
+++ src/logica/OrdenCompra.java
@@ -1,5 +1,74 @@
 package logica;
 
-public class OrdenCompra {
+import java.util.Date;
+import java.util.ArrayList;
+import java.util.List;
 
+public class OrdenCompra {
+    
+    
+    static int codigo;
+    private String estado;
+    private Date fecha;
+    static float impuesto;
+    private int numeroOrden;
+    private List<LineaOrden> lineas;
+    
+    // Constructor
+    public OrdenCompra(int numeroOrden) {
+        this.numeroOrden = numeroOrden;
+        OrdenCompra.codigo = numeroOrden;
+        this.fecha = new Date();
+        this.estado = "INICIADA";
+        this.lineas = new ArrayList<>();
+    }
+    
+    // Getters y Setters
+    public static int getCodigo() {
+        return codigo;
+    }
+    
+    public static void setCodigo(int codigo) {
+        OrdenCompra.codigo = codigo;
+    }
+    
+    public String getEstado() {
+        return estado;
+    }
+    
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+    
+    public Date getFecha() {
+        return fecha;
+    }
+    
+    public void setFecha(Date fecha) {
+        this.fecha = fecha;
+    }
+    
+    public static float getImpuesto() {
+        return impuesto;
+    }
+    
+    public static void setImpuesto(float impuesto) {
+        OrdenCompra.impuesto = impuesto;
+    }
+    
+    public int getNumeroOrden() {
+        return numeroOrden;
+    }
+    
+    public void setNumeroOrden(int numeroOrden) {
+        this.numeroOrden = numeroOrden;
+    }
+    
+    public List<LineaOrden> getLineas() {
+        return lineas;
+    }
+    
+    public void setLineas(List<LineaOrden> lineas) {
+        this.lineas = lineas;
+    }
 }
