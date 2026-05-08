d317e691cbd968618dc7c2c7131957020308461b
Jaquem <jaquemog2017@gmail.com>
2026-05-07 21:52:08 -0600

    Agregamos atributos, getters y setter.

--- src/logica/LineaOrden.java
+++ src/logica/LineaOrden.java
@@ -1,5 +1,42 @@
 package logica;
 
 public class LineaOrden {
-
+	private int numeroLinea;
+	private int cantidad;
+	private Producto producto;
+	private double precioUnitario;
+	private double subtotal;
+	
+	
+	// Constructor
+	public LineaOrden(int numeroLinea, Producto producto, int cantidad, double precioUnitario) {
+		this.numeroLinea = numeroLinea;
+		this.producto = producto;
+		this.cantidad = cantidad;
+	    this.precioUnitario = precioUnitario;
+	   this.subtotal = cantidad * precioUnitario;
+	}
+	
+	
+	//Getters y setters
+	 public int getNumeroLinea() { return numeroLinea; }
+	    public void setNumeroLinea(int numeroLinea) { this.numeroLinea = numeroLinea; }
+	    
+	    public Producto getProducto() { return producto; }
+	    public void setProducto(Producto producto) { this.producto = producto; }
+	    
+	    public int getCantidad() { return cantidad; }
+	    public void setCantidad(int cantidad) { 
+	        this.cantidad = cantidad; 
+	        this.subtotal = this.cantidad * this.precioUnitario;
+	    }
+	    
+	    public double getPrecioUnitario() { return precioUnitario; }
+	    public void setPrecioUnitario(double precioUnitario) { 
+	        this.precioUnitario = precioUnitario;
+	        this.subtotal = this.cantidad * this.precioUnitario;
+	    }
+	    
+	    public double getSubtotal() { return subtotal; }
+	
 }
