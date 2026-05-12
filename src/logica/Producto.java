6066e8d73cf7cfdf42e25ea90e2533984675131e
Jaquem <jaquemog2017@gmail.com>
2026-05-11 19:04:15 -0600

    Completo metodos de Producto

--- src/logica/Producto.java
+++ src/logica/Producto.java
@@ -1,5 +1,8 @@
 package logica;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Producto {
 	static int codigo;
 	private  int codigoProducto;
@@ -74,5 +77,51 @@ public class Producto {
 		 this.unidad = unidad; 
 		 }
 	    
+	 
+	 
+	 //Métodos
+	  
+	    public boolean actualizarProducto(int codigo, String nombre, float existencia, String unidad, double precio) {
+	        if (this.codigo == codigo || this.codigoProducto == codigo) {
+	            this.nombre = nombre;
+	            this.existencia = existencia;
+	            this.unidad = unidad;
+	            this.precio = precio;
+	            return true;
+	        }
+	        return false;
+	    }
+	    
+	    public boolean borrarProducto(int codigo) {
+	        return (this.codigo == codigo || this.codigoProducto == codigo);
+	    }
+	    
+	    public boolean crearProducto(String nombre, Float existencia, String unidad, double precio) {
+	        this.nombre = nombre;
+	        this.existencia = existencia != null ? existencia : 0;
+	        this.unidad = unidad;
+	        this.precio = precio;
+	        return true;
+	    }
+	    
+	    public Producto obtenerDatos(int codigo) {
+	        if (this.codigo == codigo || this.codigoProducto == codigo) {
+	            return this;
+	        }
+	        return null;
+	    }
+	    
+	    public List<Producto> obtenerListado() {
+	        List<Producto> lista = new ArrayList<>();
+	        lista.add(this);
+	        return lista;
+	    }
+	    
+	    @Override
+	    public String toString() {
+	        return "Producto{" + "codigo=" + codigo + ", nombre=" + nombre + 
+	               ", precio=" + precio + ", existencia=" + existencia + '}';
+	    }
+	 
 	
 }
